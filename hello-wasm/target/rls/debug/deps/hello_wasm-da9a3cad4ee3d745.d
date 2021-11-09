9765b2ae68158c28b0d7a367c9140336ba093212
nero-15 <nero15.dev@gmail.com>
2021-11-09 15:17:50 +0900

    new lib

---
+++ hello-wasm/target/rls/debug/deps/hello_wasm-da9a3cad4ee3d745.d
@@ -0,0 +1,5 @@
+c:\Users\tak\devel\web-assembly-learning-app\hello-wasm\target\rls\debug\deps\hello_wasm-da9a3cad4ee3d745.rmeta: src\lib.rs
+
+c:\Users\tak\devel\web-assembly-learning-app\hello-wasm\target\rls\debug\deps\hello_wasm-da9a3cad4ee3d745.d: src\lib.rs
+
+src\lib.rs:
